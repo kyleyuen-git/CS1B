0159a4b61caffa281be596457dd6b284dd8cddae
Kyle Yuen <kyleyuen0@gmail.com>
2025-07-28 00:14:27 -0700

    hw11 creation // nothing important

---
+++ HW_11/UnsortedList.h
@@ -0,0 +1,17 @@
+# pragma once
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+static const int MAX_SIZE = 10;
+
+class UnsortedList
+{
+private:
+    int numbers;
+
+public:
+
+};
